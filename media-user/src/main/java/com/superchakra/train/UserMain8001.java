e3ec69ba9b627d121113c86069fa75c60cb9e35d
superchakra <1905046474@qq.com>
2024-06-14 15:00:32 +0800

    init project

---
+++ media-user/src/main/java/com/superchakra/train/UserMain8001.java
@@ -0,0 +1,11 @@
+package com.superchakra.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.superchakra.train")
+public class UserMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(UserMain8001.class,args);
+    }
+}
